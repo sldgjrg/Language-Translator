2425d7da8b60e34cc86904bf4c1dba3989913f0f
sldgjrg <126630199+sldgjrg@users.noreply.github.com>
2023-11-06 23:32:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
+from flask import Flask, render_template, request
 import speech_recognition as sr
 from googletrans import Translator
+
+app = Flask(__name__)
 
 # Initialize the recognizer and translator
 recognizer = sr.Recognizer()
 translator = Translator()
 
-def listen_and_translate():
-   # Use the microphone as the audio source
-   with sr.Microphone() as source:
-       print("Listening...")
-       audio = recognizer.listen(source)
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-   try:
-       # Recognize the speech using Google Speech Recognition
-       print("Recognizing...")
-       text = recognizer.recognize_google(audio, language='jp-JP')   # If you want to use other languages, change the language code. For example, Urdu is ur-PK.
-       print(f"You said: {text}\n")
+@app.route('/translate', methods=['POST'])
+def translate():
+    language_code = request.form['language']
+    
+    # Use the microphone as the audio source
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
 
-       # Translate the text into English
-       translation = translator.translate(text, dest='en')
-       print(f"Translation: {translation.text}\n")
+    try:
+        # Recognize the speech using Google Speech Recognition
+        print("Recognizing...")
+        text = recognizer.recognize_google(audio, language=f'{language_code}-{language_code.upper()}')
+        print(f"You said: {text}\n")
 
-   except Exception as e:
-       print("Error:", e)
+        # Translate the text into English
+        translation = translator.translate(text, dest='en')
+        translated_text = translation.text
+        print(f"Translation: {translated_text}\n")
 
-# Keep listening and translating in a loop
-while True:
-   listen_and_translate()
+    except Exception as e:
+        translated_text = f"Error: {e}"
+
+    return render_template('index.html', translated_text=translated_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
